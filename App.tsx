7600fdae90c90a76e9cd19bc7e8f37ed88d46d94
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2023-07-08 22:50:55 +0800

    main basic layout

--- App.tsx
+++ App.tsx
@@ -1,35 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>GibTalk - Alpha</Text>
       </View>
-      
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.body}>
+        <View style={styles.bodyTop}>
+          <View style={styles.current}>
+            <Text style={styles.currentText}>Drag downward to remove</Text>
+          </View>
+
+          <View style={styles.controls}>
+            <Text>Play</Text>
+            <Text>Clear</Text>
+          </View>
+
+        </View>
+
+        <View style={styles.bodyBottom}>
+          <Text>Body</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    alignItems: 'stretch',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1,
     justifyContent: 'flex-start',
   },
   header: {
-    flexDirection: 'row',
-    backgroundColor: 'black',
-    height: '6%',
-    alignSelf: 'stretch',
-    paddingHorizontal: 5,
     alignItems: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    height: '6%',
+    paddingHorizontal: 5,
   },
   headerText: {
     color: 'white'
+  },
+  body: {
+    backgroundColor: 'orange',
+    flex: 1,
+    padding: 5,
+  },
+  bodyTop: {
+    flexDirection: 'row',
+    height: '15%',
+  },
+  current: {
+    backgroundColor: 'rgba(100, 100, 100, 0.5)',
+    flex: 1,
+  },
+  currentText: {
+    alignSelf: 'center',
+    marginTop: '2%',
+    position: 'absolute',
+  },
+  controls: {
+    backgroundColor: 'rgba(100, 100, 100, 0.5)',
+    flexDirection: 'row',
+    marginLeft: 5,
+  },
+  bodyBottom: {
+    backgroundColor: 'rgba(100, 100, 100, 0.5)',
+    flex: 1,
+    marginTop: 5,
   },
 });
