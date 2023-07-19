1d0bdcf59bacf07eaf18542dffc91e125268f2be
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2023-07-20 00:18:10 +0800

    simplify wordsReducer recursive call

--- src/service/words.tsx
+++ src/service/words.tsx
@@ -33,115 +33,63 @@ export const WordsContext = createContext([]);
 export const WordsDispatchContext = createContext(null);
 
 export function wordsReducer(words: Word[], action) {
+  if (action.wordPath && action.wordPath.length) {
+    const [p, ...wordPath] = action.wordPath;
+    const i = words.findIndex((w) => w.id === p);
+
+    const newWords = words.slice();
+    newWords[i].children = wordsReducer(words[i].children, {
+      ...action,
+      wordPath,
+    });
+
+    return newWords;
+  }
+
   switch (action.type) {
     case "set-words": {
       return action.words;
     }
     case "add-word": {
-      if (!action.wordPath.length) {
-        return [...words, action.word];
-      } else {
-        const [p, ...wordPath] = action.wordPath;
-        const i = words.findIndex((w) => w.id === p);
-
-        const newWords = words.slice();
-        newWords[i].children = wordsReducer(words[i].children, {
-          ...action,
-          wordPath,
-        });
-
-        return newWords;
-      }
+      return [...words, action.word];
     }
     case "update-word": {
-      if (!action.wordPath.length) {
-        const i = words.findIndex((w) => w.id === action.word.id);
-        const newWords = words.slice();
-        newWords[i] = action.word;
-        return newWords;
-      } else {
-        const [p, ...wordPath] = action.wordPath;
-        const i = words.findIndex((w) => w.id === p);
-
-        const newWords = words.slice();
-        newWords[i].children = wordsReducer(words[i].children, {
-          ...action,
-          wordPath,
-        });
-
-        return newWords;
-      }
+      const i = words.findIndex((w) => w.id === action.word.id);
+      const newWords = words.slice();
+      newWords[i] = action.word;
+      return newWords;
     }
     case "remove-word": {
-      if (!action.wordPath.length) {
-        return words.filter((w) => w.id !== action.wordId);
-      } else {
-        const [p, ...wordPath] = action.wordPath;
-        const i = words.findIndex((w) => w.id === p);
-
-        const newWords = words.slice();
-        newWords[i].children = wordsReducer(words[i].children, {
-          ...action,
-          wordPath,
-        });
-
-        return newWords;
-      }
+      return words.filter((w) => w.id !== action.wordId);
     }
     case "move-word-left": {
-      if (!action.wordPath.length) {
-        const i = words.findIndex((w) => w.id === action.wordId);
-        if (i === 0 || i === -1) {
-          return words;
-        }
-
-        const target = words[i];
-        const left = words[i - 1];
-
-        const newWords = words.slice();
-        newWords[i - 1] = target;
-        newWords[i] = left;
-        return newWords;
-      } else {
-        const [p, ...wordPath] = action.wordPath;
-        const i = words.findIndex((w) => w.id === p);
-
-        const newWords = words.slice();
-        newWords[i].children = wordsReducer(words[i].children, {
-          ...action,
-          wordPath,
-        });
-
-        return newWords;
+      const i = words.findIndex((w) => w.id === action.wordId);
+      if (i === 0 || i === -1) {
+        return words;
       }
+
+      const target = words[i];
+      const left = words[i - 1];
+
+      const newWords = words.slice();
+      newWords[i - 1] = target;
+      newWords[i] = left;
+      return newWords;
     }
     case "move-word-right": {
-      if (!action.wordPath.length) {
-        const i = words.findIndex((w) => w.id === action.wordId);
-        if (i === words.length - 1) {
-          return words;
-        }
-
-        const target = words[i];
-        const right = words[i + 1];
-
-        const newWords = words.slice();
-        newWords[i + 1] = target;
-        newWords[i] = right;
-
-        return newWords;
-      } else {
-        const [p, ...wordPath] = action.wordPath;
-        const i = words.findIndex((w) => w.id === p);
-
-        const newWords = words.slice();
-        newWords[i].children = wordsReducer(words[i].children, {
-          ...action,
-          wordPath,
-        });
-
-        return newWords;
+      const i = words.findIndex((w) => w.id === action.wordId);
+      if (i === words.length - 1) {
+        return words;
       }
+
+      const target = words[i];
+      const right = words[i + 1];
+
+      const newWords = words.slice();
+      newWords[i + 1] = target;
+      newWords[i] = right;
+
+      return newWords;
     }
     default: {
       throw Error("Unknown action: " + action.type);
