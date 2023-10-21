2930d787c998fd72743ca83a52f247deedc91ced
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2023-10-21 17:08:56 +0800

    error alert on data read error

--- src/appState/storage.ts
+++ src/appState/storage.ts
@@ -1,18 +1,47 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
 import { useState, useEffect } from "react";
 
 async function storeData<T>(key: string, updatedData: T): Promise<void> {
-  const jsonValue = JSON.stringify(updatedData);
-  await AsyncStorage.setItem(key, jsonValue);
+  try {
+    const jsonValue = JSON.stringify(updatedData);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (error) {
+    console.log(error);
+    let message =
+      error instanceof Error
+        ? `${error.name} - ${error.message}`
+        : "Unknown error";
+
+    Alert.alert("Store Data Error", message, [
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+
+    throw error;
+  }
 }
 
 async function readData<T>(key: string): Promise<T | null> {
-  const jsonValue = await AsyncStorage.getItem(key);
-  if (!jsonValue) {
-    return null;
-  }
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    if (!jsonValue) {
+      return null;
+    }
 
-  return JSON.parse(jsonValue) as T;
+    return JSON.parse(jsonValue) as T;
+  } catch (error) {
+    console.log(error);
+    let message =
+      error instanceof Error
+        ? `${error.name} - ${error.message}`
+        : "Unknown error";
+
+    Alert.alert("Read Data Error", message, [
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+
+    throw error;
+  }
 }
 
 export function useStorage<T>(
